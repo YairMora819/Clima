8b7d6db7cd875197b5f3af2bede885c3154921c7
YairMora819 <yairmora819@gmail.com>
2025-07-15 15:41:02 -0600

    Clima

--- app/src/main/java/com/example/revisiondeclima/WeatherResponse.kt
+++ app/src/main/java/com/example/revisiondeclima/WeatherResponse.kt
@@ -3,18 +3,25 @@ package com.example.revisiondeclima
 data class WeatherResponse(
     val main: Main,
     val weather: List<Weather>,
-    val name: String
+    val name: String,
+    val coord: Coord
 )
 
 data class Main(
     val temp: Float,
-    val humidity: Int
+    val humidity: Int,
+    val feelsLike: Float? = null
 )
 
 data class Weather(
     val main: String,
     val description: String,
     val icon: String
+)
+
+data class Coord(
+    val lat: Double,
+    val lon: Double
 )
 
 // Para pronóstico extendido (One Call API)
